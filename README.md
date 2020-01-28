876563441e308f96f1a0f7a1e24d640c33ad8b57
ichynul <ichynul@outlook.com>
2020-01-28 13:58:41 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-lyatadmin
+light-year-admin-template
 
 后台模板页面及样式使用的是【Light-Year-Admin-Template】http://lyear.itshubao.com/
--- src/common/hooks/Setup.php
+++ src/common/hooks/Setup.php
@@ -12,16 +12,7 @@ class Setup
         $module = Request::module();
 
         if ($module == 'admin') { //admin模块， 替换错误和跳转模板
-
-            $instance = Plugin::getInstance();
-
-            $rootPath = $instance->getRoot();
-
-            $tplPath = $rootPath . implode(DIRECTORY_SEPARATOR, ['src', 'admin', 'view', 'tpl', '']);
-
-            config('exception_tmpl', $tplPath . 'exception_tmpl.tpl');
-            config('dispatch_success_tmpl', $tplPath . 'dispatch_jump.tpl');
-            config('dispatch_error_tmpl', $tplPath . 'dispatch_jump.tpl');
+            
         }
     }
 }
